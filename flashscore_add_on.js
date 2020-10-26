be2f4148d563bdb0df66139a1ea23aa7df05ebc8
Daniel Silva <danielsilva@Daniels-iMac.home>
2020-10-26 03:33:08 +0000

    display change v0.5

--- flashscore_add_on.js
+++ flashscore_add_on.js
@@ -12,9 +12,13 @@
 (function() {
     'use strict';
 
+    document.getElementsByClassName("container__content content")[0].style.width = "1150px";
+    var space = document.getElementById('rc-top');
+
+    space.style.width = "400px";
     setInterval(function(){
 
-        document.getElementById('box_over_content_11010').innerHTML = "";
+        space.innerHTML = "";
         var lGames = document.getElementsByClassName("event__match event__match--live event__match--oneLine");
 
         for (var i = 0; i < lGames.length; i++){
@@ -28,12 +32,12 @@
         var hCardTeam = hTeam.getElementsByClassName("card___2ip_DLm icon--redCard").length;
         var aCardTeam = aTeam.getElementsByClassName("card___2ip_DLm icon--redCard").length;
 
-            if(hCardTeam > 0 || aCardTeam > 0){
+            //if(hCardTeam > 0 || aCardTeam > 0){
                 hTeam = (hCardTeam > 0)? "<span style='color:orange;'>"+hTeam.textContent+"</span>" : hTeam.textContent ;
                 aTeam = (aCardTeam > 0)? "<span style='color:orange;'>"+aTeam.textContent+"</span>" : aTeam.textContent ;
 
-                document.getElementById('box_over_content_11010').innerHTML += "<a href='#"+lGames[i].id+"'><p style='color:white;padding:2px;font-size:10pt;'><font style='border-radius:10%;width:15px;height:10px;background-color:red;'><b>"+hCardTeam+"</b></font> "+hTeam+" "+score+" "+aTeam+" <font style='border-radius:10%;width:10px;height:10px;background-color:red;'><b>"+aCardTeam+"</b></font> | Time: "+time+"</p></a>";
-            }
+                space.innerHTML += "<a href='#"+lGames[i].id+"'><p style='color:white;padding:2px;font-size:10pt;'><font style='border-radius:10%;width:15px;height:10px;background-color:red;'><b>"+hCardTeam+"</b></font> "+hTeam+" "+score+" "+aTeam+" <font style='border-radius:10%;width:10px;height:10px;background-color:red;'><b>"+aCardTeam+"</b></font> | Time: "+time+"</p></a>";
+            //}
         }
    }, 3000);
 
